a6fd99310a9ccb0d09474fb3f689cf3b685fa45f
Maheshron <28363613+Maheshron@users.noreply.github.com>
2022-09-09 00:06:05 +0530

    new

---
+++ react/design/article-preview-component-master/script.js
@@ -0,0 +1,10 @@
+const tooltip = document.getElementById('tooltip');
+
+const share = document.getElementById('share');
+
+//share.addEventListener('click',tooltipShow());
+
+function tooltipShow(){
+   
+    tooltip.classList.toggle('show');
+}
